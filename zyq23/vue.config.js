432fd91036003e298d152ea16a7d7495fc42d61b
CheeseburgerIM <13051172893@163.com>
2022-09-13 21:54:50 +0800

    Site updated: 2022-09-13 21:54:50

--- zyq23/vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/PeanutGirl23/zyq23/dist/',
-  lintOnSave: false
-})
